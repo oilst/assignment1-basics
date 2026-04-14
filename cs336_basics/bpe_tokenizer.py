356c0b8d3621cc9bf6d6805486d95e2c21e7f427
uelipeter <ueli.peter@gmail.com>
2026-04-14 22:19:57 +0200

    Implemented parts of the tokenizer

---
+++ cs336_basics/bpe_tokenizer.py
@@ -0,0 +1,46 @@
+from abc import ABC
+from dataclasses import dataclass
+
+def merge(indices: list[int], pair: tuple[int, int], new_index: int) -> list[int]:
+    """Return `indices`, but with all instances of `pair` replaced with `new_index`."""
+    new_indices = []
+    i = 0
+    while i < len(indices):
+        if i + 1 < len(indices) and indices[i] == pair[0] and indices[i + 1] == pair[1]:
+            new_indices.append(new_index)
+            i += 2
+        else:
+            new_indices.append(indices[i])
+            i += 1
+    return new_indices
+
+class Tokenizer(ABC):
+    """Abstract interface for a tokenizer."""
+    def encode(self, string: str) -> list[int]:
+        raise NotImplementedError
+    def decode(self, indices: list[int]) -> str:
+        raise NotImplementedError
+
+@dataclass(frozen=True)
+class BPETokenizerParams:
+    """All you need to specify a BPETokenizer."""
+    vocab: dict[int, bytes]     # index -> bytes
+    merges:  list[tuple[bytes, bytes]]
+    special_tokens: list[str]
+
+class BPETokenizer(Tokenizer):
+    """BPE tokenizer given a set of merges and a vocabulary."""
+    def __init__(self, params: BPETokenizerParams):
+        self.params = params
+    def encode(self, string: str) -> list[int]:
+        # Note: this is a very slow implementation
+        bytes_to_indices = {value: key for key, value in self.params.vocab.items()}
+        indices = [bytes_to_indices[bytes([byte])] for byte in string.encode("utf-8")]
+        for new_index, pair in enumerate(self.params.merges):
+            idx_pair = (bytes_to_indices[pair[0]], bytes_to_indices[pair[1]])
+            indices = merge(indices, idx_pair, new_index + 256)
+        return indices
+    def decode(self, indices: list[int]) -> str:
+        bytes_list = list(map(self.params.vocab.get, indices))
+        string = b"".join(bytes_list).decode("utf-8")
+        return string
--- tests/adapters.py
+++ tests/adapters.py
@@ -9,6 +9,8 @@ import torch
 from jaxtyping import Bool, Float, Int
 from torch import Tensor
 
+from cs336_basics.bpe_tokenizer import BPETokenizerParams, BPETokenizer
+
 
 def run_linear(
     d_in: int,
@@ -559,7 +561,9 @@ def get_tokenizer(
     Returns:
         A BPE tokenizer that uses the provided vocab, merges, and special tokens.
     """
-    raise NotImplementedError
+    merges_dict = {pair:i+255 for i, pair in enumerate(merges)}
+    params = BPETokenizerParams(vocab=vocab, merges=merges, special_tokens=special_tokens)
+    return BPETokenizer(params)
 
 
 def run_train_bpe(
